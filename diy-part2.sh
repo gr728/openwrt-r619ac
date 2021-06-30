73d015ddab109cb9fb32585a3d97d19a4f34a3bf
gr728 <62072234+gr728@users.noreply.github.com>
2021-06-30 10:25:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.20.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 echo '添加登录密码密文'
 sed -i 's/root::0:0:99999:7:::/root:$1$FHLZsCKT$LvLemPCPOEofaV5pyBWhP0:0:0:99999:7:::/g'  package/base-files/files/etc/shadow
